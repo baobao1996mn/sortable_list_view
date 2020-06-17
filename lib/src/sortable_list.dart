ab4c91966adac243fa3a197e07218587d6266a50
baothg-simplelove <baothg@cnv.vn>
2020-06-18 02:50:41 +0700

    sort by list view

--- lib/src/sortable_list.dart
+++ lib/src/sortable_list.dart
@@ -1,25 +1,49 @@
 import 'package:flutter/material.dart';
 
-
-class SortableListView extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() => _SortableListViewState();
+class SortableController<T> {
+  Function(List<T> items) _setItems;
+  void setItems(List<T> items) => _setItems(items);
 }
 
-class _SortableListViewState extends State<SortableListView> {
+class SortableListView<T> extends StatefulWidget {
+  final SortableController<T> controller;
+  final bool Function(T t1, T t2) compare;
+  final List<T> initial;
+  final IndexedWidgetBuilder itemBuilder;
+
+  const SortableListView(
+      {Key key,
+      @required this.compare,
+      @required this.controller,
+      @required this.initial,
+      @required this.itemBuilder})
+      : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _SortableListViewState<T>();
+}
+
+class _SortableListViewState<T> extends State<SortableListView> {
+  List<T> _items;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    this._items = widget.initial;
+    this.widget.controller._setItems = _setItems;
+  }
+
+  void _setItems(List<T> items) {
+    setState(() {
+      this._items = items;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return ListView.separated(
-      itemCount: 20,
-      itemBuilder: (context, index) {
-        return ListTile(
-          leading: CircleAvatar(child: Text("$index")),
-          title: Text("Item $index"),
-        );
-      },
-      separatorBuilder: (BuildContext context, int index) {
-        return Divider();
-      },
+    return ListView.builder(
+      itemCount: _items.length,
+      itemBuilder: widget.itemBuilder,
     );
   }
 }
